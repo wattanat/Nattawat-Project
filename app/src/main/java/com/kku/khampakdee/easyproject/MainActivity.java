9f99b390ef29ad59a7c3dfb9a84829ebe9769dd6
CSITGIS <k.nattawat@kkumail.com>
2016-11-12 12:04:46 +0700

    Intent to SignUp finish

--- app/src/main/java/com/kku/khampakdee/easyproject/MainActivity.java
+++ app/src/main/java/com/kku/khampakdee/easyproject/MainActivity.java
@@ -1,13 +1,35 @@
 package com.kku.khampakdee.easyproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity { // Main Class
+
+    // Explicit
+    private Button signInButton, signUpButton;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { // Main Method
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        // Bind Widget
+
+        signInButton = (Button) findViewById(R.id.button2);
+        signUpButton = (Button) findViewById(R.id.button);
+
+        // Sign up Controller
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, signUpButton.class));
+            }
+        });
+
+
+    } // Main Method
+} // Main Class
